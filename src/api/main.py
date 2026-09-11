037df2016b17fc352d6f33961a4e9fb1cdda5ad6
unknown <farhatchaudary34@gmail.com>
2026-09-11 15:59:15 +0500

    Add EDA notebook and FastAPI prediction endpoint

---
+++ src/api/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+model = joblib.load('../mode/fraud_model.pkl')
+scaler = joblib.load('../mode/scaler.pkl')
+
+print("Model and scaler loaded successfully!")
+
+
+class Transaction(BaseModel):
+    features: List[float]
+
+
+@app.post("/predict")
+def predict(transaction: Transaction):
+    data = np.array(transaction.features).reshape(1, -1)
+    data_scaled = scaler.transform(data)
+    prediction = model.predict(data_scaled)
+    probability = model.predict_proba(data_scaled)
+
+    result = "Fraud" if prediction[0] == 1 else "Not Fraud"
+
+    return {
+        "prediction": result,
+        "fraud_probability": float(probability[0][1])
+    }
